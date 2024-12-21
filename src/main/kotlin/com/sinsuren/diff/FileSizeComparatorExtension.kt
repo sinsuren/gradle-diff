8a5a7f51da48a5d25fd554a1f872cf296124c582
Surendra Singh <surendra.singh@cred.club>
2024-12-21 18:42:07 +0530

    Multiple configs

--- src/main/kotlin/com/sinsuren/diff/FileSizeComparatorExtension.kt
+++ src/main/kotlin/com/sinsuren/diff/FileSizeComparatorExtension.kt
@@ -1,7 +1,17 @@
 package com.sinsuren.diff
 
-// Extension class
 open class FileSizeComparatorExtension {
+    // List of file path configurations
+    var fileComparisons: MutableList<FileComparisonConfig> = mutableListOf()
+
+    fun fileComparison(configure: FileComparisonConfig.() -> Unit) {
+        val comparison = FileComparisonConfig()
+        comparison.configure()
+        fileComparisons.add(comparison)
+    }
+}
+
+open class FileComparisonConfig {
     var inputFilePath1: String? = null
     var inputFilePath2: String? = null
     var outputFilePath: String? = null
--- src/main/kotlin/com/sinsuren/diff/FileSizeComparatorPlugin.kt
+++ src/main/kotlin/com/sinsuren/diff/FileSizeComparatorPlugin.kt
@@ -5,14 +5,21 @@ import org.gradle.api.Project
 
 class FileSizeComparatorPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        // Create an extension to hold configuration
         val extension = project.extensions.create("fileSizeComparator", FileSizeComparatorExtension::class.java)
 
-        // Register the task and configure it using the extension
-        project.tasks.register("compareFileSize", CompareFileSizeTask::class.java) { task ->
-            task.inputFilePath1 = extension.inputFilePath1
-            task.inputFilePath2 = extension.inputFilePath2
-            task.outputFilePath = extension.outputFilePath
+        project.afterEvaluate {
+            extension.fileComparisons.forEachIndexed { index, config ->
+                val taskName = "compareFileSizeTask$index"
+
+                project.tasks.register(taskName, CompareFileSizeTask::class.java) { task ->
+                    task.group = "File Comparison"
+                    task.description = "Compares file sizes for configuration $index."
+                    task.inputFilePath1 = config.inputFilePath1
+                    task.inputFilePath2 = config.inputFilePath2
+                    task.outputFilePath = config.outputFilePath
+                }
+            }
         }
     }
 }
+
